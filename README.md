829b9ed28575fbec5d490ac22dc7f862708af9a7
Hamzah Fayad <hamzahfayad99@gmail.com>
2021-08-29 11:56:51 +0200

    preprare for theme switch

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
-# Devfinder 
+# Devfinder
 
 The challenge is to build out a GitHub user search app using the GitHub users API.
 
 The users should be able to:
 
-* View the optimal layout for the app depending on their device's screen size
-* See hover states for all interactive elements on the page
-* Search for GitHub users by their username
-* See relevant user information based on their search
-* Switch between light and dark themes
-* Bonus: Have the correct color scheme chosen for them based on their computer preferences.
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Search for GitHub users by their username
+- See relevant user information based on their search
+- Switch between light and dark themes
+- Bonus: Have the correct color scheme chosen for them based on their computer preferences.
+
+Icons from https://fontawesome.com | https://fontawesome.com/license
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,110 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="head">
+      <div>
+        <h1>devfinder</h1>
+      </div>
+
+      <div
+        style="cursor: pointer"
+        class="toggle"
+        aria-label="Toggle themes"
+        @click="toggleTheme"
+      >
+        <div class="dark-light">
+          <span v-if="this.theme == 'darkMode'"> Light</span>
+          <span v-else> Dark</span>
+        </div>
+        <div class="dark-light--icon">
+          <img
+            v-if="this.theme == 'darkMode'"
+            src="./assets/sun-regular.svg"
+            alt=""
+          />
+          <img v-else src="./assets/moon-regular.svg" alt="" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+  data() {
+    return {
+      theme: "",
+    };
+  },
+  mounted() {
+    document.documentElement.setAttribute("data-theme", this.theme);
+  },
+  methods: {
+    toggleTheme() {
+      this.theme = this.theme == "darkMode" ? "" : "darkMode";
+      document.documentElement.setAttribute("data-theme", this.theme);
+    },
+  },
+};
 </script>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Rokkitt:wght@300;400&display=swap");
+
+:root {
+  --background-color: #e7f2ff;
+  --text-color: #1c1c1c;
+  --cards-color: #fff;
+  --accent-color: #0679fb;
+}
+[data-theme="darkMode"] {
+  --background-color: #141c30;
+  --text-color: #fff;
+  --cards-color: #1f2a48;
+}
+html,
+body {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Rokkitt", serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: var(--background-color);
+  color: var(--text-color);
+  padding: 60px 120px;
+  height: 100vh;
+}
+h1 {
+  padding: 0;
+  margin: 0;
+}
+.head {
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-around;
+  align-items: center;
+}
+.toggle {
+  display: flex;
+  flex-flow: row nowrap;
+  gap: 12px;
+  align-items: flex-end;
+  .dark-light {
+    text-transform: uppercase;
+  }
+  .dark-light--icon {
+    width: 18px;
+    height: 18px;
+    color: var(--text-color);
+    transition: 250ms;
+    &:hover {
+      transform: scale(1.2);
+    }
+  }
 }
 </style>
